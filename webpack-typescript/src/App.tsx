a9b734f1b69f10d048c794b296de1229c755412a
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-25 23:42:01 +0530

    chore: Add button and counter functionality to App component
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- webpack-typescript/src/App.tsx
+++ webpack-typescript/src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Image from "./Frame.svg";
+import { useState } from "react";
 
 export const App = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <>
-    <h1>React TS</h1>
-    <img src={Image} width={400} height={300}/>
+      <h1>React TS - Counter App</h1>
+      <button onClick={() => setCount(count + 1)}>Count -  {count}</button>
+      <img src={Image} width={400} height={300} />
     </>
   );
 };
