1514b205eadf221d1e315b0c916af1e42997b1c1
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-25 23:34:36 +0530

    created separate webpack configuration files for prod and development
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- webpack-typescript/webpack/webpack.config.js
+++ webpack-typescript/webpack/webpack.config.js
@@ -1,39 +1,12 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require("webpack-merge")
+const commonConfig = require("./webpack.common");
 
-module.exports = {
-    entry: path.resolve('./src/index.tsx'),
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(png|svg|gif|jpg|jpeg)$/,
-                type: "asset/resource"
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ],
-    },
-    output: {
-        path: path.resolve(__dirname, '..', 'build'),
-        filename: '[name].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', 'src/index.html'),
-        }),
-    ],
+
+module.exports = (envVars) => {
+    const { env } = envVars;
+    const envConfig = require(`./webpack.${env}.js`);
+
+    const config = merge(commonConfig, envConfig);
+
+    return config;
 }
