6990f83908347d95292f98008230e2344dc2241c
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-25 23:42:19 +0530

    chore: Add React Refresh plugin for hot module replacement in development mode
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- webpack-typescript/webpack/webpack.dev.js
+++ webpack-typescript/webpack/webpack.dev.js
@@ -1,4 +1,13 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
     mode: "development",
-    devtool: "cheap-module-source-map"
+    devtool: "cheap-module-source-map",
+    plugins: [
+        new ReactRefreshWebpackPlugin()
+    ],
+    devServer: {
+        hot: true,
+        open: true
+    }
 }
