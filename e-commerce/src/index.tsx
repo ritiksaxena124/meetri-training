08c55d9250b8a16521b788ad0759178a94ef4036
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-27 23:19:27 +0530

    feat: Add Redux Persist for cart state persistence
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/src/index.tsx
+++ e-commerce/src/index.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 const rootEle = document.getElementById("root");
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 if (rootEle) {
   createRoot(rootEle).render(
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   );
 } else {
