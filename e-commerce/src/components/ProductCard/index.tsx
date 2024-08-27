f1592e27b31b0649aa6c1b2ae1f5e122304c476f
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-27 10:15:42 +0530

    feat: Add filtering and sorting functionality
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/src/components/ProductCard/index.tsx
+++ e-commerce/src/components/ProductCard/index.tsx
@@ -2,14 +2,18 @@ import { ProductType } from "../../pages/Home";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   return (
-    <div className="max-w-sm border p-4 rounded-md space-y-4">
+    <div className="max-w-md border p-4 rounded-md space-y-4 flex flex-col gap-4">
       <h1 className="text-lg text-zinc-100">{product?.title}</h1>
-      <p className="text-zinc-400">
-        {`${product?.description?.substr(0, 100)}...`}...
-      </p>
+      <p className="text-zinc-400 h-full">{`${product?.description.slice(0, 100)}...`}</p>
       <div className="flex items-center justify-between">
-        <span className="text-xl font-semibold">${product.price}</span>
-        <div>{product.rating.rate}⭐</div>
+        <span className="text-xl font-semibold">${product?.price}</span>
+        <div>{product?.rating?.rate}⭐</div>
+      </div>
+
+      <div>
+        <button className="bg-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-900 font-medium mt-auto">
+          Add to cart
+        </button>
       </div>
     </div>
   );
--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import Loader from "../components/Loader";
 import ProductCard from "../components/ProductCard";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 import client from "../utils/axios";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { addToCart, emptyCart } from "../store/slices/cartSlice";
 
 export interface ProductType {
   id: number;
@@ -18,20 +20,62 @@ export interface ProductType {
 }
 
 const Home: React.FC = () => {
+  const [filteredData, setFilteredData] = useState<ProductType[]>([]);
   const [products, setProducts] = useState<ProductType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-
-  async function getProductsData(setProducts: (val: ProductType[]) => void) {
-    setLoading(true);
-    const response = await client.get("/products");
-    setProducts(response?.data);
-    setLoading(false);
-    console.log(response.data);
-  }
+  const [search, setSearch] = useState<string>("");
 
   useEffect(() => {
-    getProductsData(setProducts);
+    async function getProductsData() {
+      setLoading(true);
+      const { data } = await client.get("/products");
+      setProducts(data);
+      setFilteredData(data);
+      setLoading(false);
+
+      console.log(data);
+    }
+
+    getProductsData();
   }, []);
+
+  const filterProductsData = (category: string) => {
+    if (category === "all") {
+      setFilteredData(products);
+    } else {
+      const data = products?.filter((product) => product.category.toLowerCase() === category.toLowerCase());
+      setFilteredData(data);
+    }
+  };
+
+  const sortByPrice = (type: string) => {
+    switch (type) {
+      case "LOW_TO_HIGH":
+        filteredData?.sort(
+          (product1, product2) => product1.price - product2.price
+        );
+        setFilteredData([...filteredData]);
+        break;
+
+      case "HIGH_TO_LOW":
+        filteredData?.sort(
+          (product1, product2) => product2.price - product1.price
+        );
+        setFilteredData([...filteredData]);
+        break;
+      default:
+        console.log("error sorting products by price");
+        break;
+    }
+  };
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    const data = products?.filter((product) =>
+      product?.title?.toLowerCase()?.includes(e.target.value)
+    );
+    setFilteredData(data);
+  };
 
   if (loading) {
     return <Loader />;
@@ -40,11 +84,76 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen p-8 space-y-8">
-        <div className="flex gap-4 basis-1/4 flex-wrap mx-auto justify-center">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+        <div className="flex items-end justify-between gap-8">
+          <div className="space-y-4 basis-1/4">
+            <h2 className="text-xl">Filter by category</h2>
+            <div className="flex gap-4">
+              <button
+                onClick={() => filterProductsData("all")}
+                className="border border-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-200 font-medium"
+              >
+                All
+              </button>
+              <button
+                onClick={() => filterProductsData("men's clothing")}
+                className="border border-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-200 font-medium"
+              >
+                Clothes
+              </button>
+
+              <button
+                onClick={() => filterProductsData("electronics")}
+                className="border border-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-200 font-medium"
+              >
+                Electronics
+              </button>
+            </div>
+          </div>
+
+          <div className="flex border border-zinc-600 rounded-md py-2 px-3 w-full">
+            <input
+              type="text"
+              name="search"
+              className="outline-none p-2 rounded-md indent w-full"
+              autoFocus
+              placeholder="Search product"
+              value={search}
+              onChange={handleSearch}
+            />
+          </div>
+
+          <div className="space-y-4 basis-1/4">
+            <h2 className="text-xl">Sort by price</h2>
+            <div className="flex gap-4">
+              <button
+                onClick={() => sortByPrice("LOW_TO_HIGH")}
+                className="border border-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-200 font-medium"
+              >
+                Low to High
+              </button>
+              <button
+                onClick={() => sortByPrice("HIGH_TO_LOW")}
+                className="border border-zinc-200 rounded-md text-sm px-3 py-2 text-zinc-200 font-medium"
+              >
+                High to Low
+              </button>
+            </div>
+          </div>
         </div>
+
+        <hr />
+        {filteredData && filteredData.length === 0 ? (
+          <div className="w-full h-[calc(100vh-220px)] flex items-center justify-center">
+            <h1 className="text-2xl text-zinc-500">No results found!</h1>
+          </div>
+        ) : (
+          <div className="flex gap-4 basis-1/4 flex-wrap mx-auto justify-center">
+            {filteredData &&
+              filteredData.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </div>
+        )}
       </div>
     </>
   );
