2b804e0084612e63a15fe6f59a7f4a827a76f851
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-28 12:25:20 +0530

    Added React router dom navigation
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,7 +1,15 @@
 import Home from "./pages/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
 const App = () => {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/product" element={<ProductPage />}/>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
---
+++ e-commerce/src/pages/ProductPage.tsx
@@ -0,0 +1,5 @@
+const ProductPage = () => {
+    return <h1>This is my product</h1>
+}
+
+export default ProductPage
