ce88d6e6ab03cf25184e8b47a7c81d0200b54193
Ritik Saxena <ritiksaxena124@gmail.com>
2024-09-03 12:27:43 +0530

    added middleware of cookie parser and enable cors
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/server/src/main.ts
+++ e-commerce/server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParse from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParse());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
