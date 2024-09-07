3709db19c16d43d19497c0d4fbb5b575a802080f
Ritik Saxena <ritiksaxena124@gmail.com>
2024-09-07 15:50:35 +0530

    verify token route added for protected route from frontend
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/server/src/auth/auth.controller.ts
+++ e-commerce/server/src/auth/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { registerUserDTO, loginUserDTO } from 'src/dto';
 import { Response } from 'express';
 import { RequestInterceptor } from 'src/interceptors/request.interceptor';
+import { AuthGuard } from './guards/auth.guard';
 
 @UseInterceptors(RequestInterceptor)
 @Controller('auth')
 export class AuthController {
   constructor(private auth: AuthService) {}
+
+  @UseGuards(AuthGuard)
+  @Get('verify-auth')
+  verifyAuth() {
+    return {
+      msg: 'User is authenticated',
+    };
+  }
 
   @Post('register')
   registerUser(@Body() dto: registerUserDTO) {
