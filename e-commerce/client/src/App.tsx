5cb429113c039bae17f89704519580efae34f121
Ritik Saxena <ritiksaxena124@gmail.com>
2024-09-07 15:49:32 +0530

    home page is now protected
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- e-commerce/client/src/App.tsx
+++ e-commerce/client/src/App.tsx
@@ -4,11 +4,16 @@ import ProductPage from "./pages/ProductPage";
 import Cart from "./components/Cart";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+          }/>
         <Route path="/product/:id" element={<ProductPage />}/>
         <Route path="/cart" element={<Cart />}/>
         <Route path="/register" element={<Register />}/>
